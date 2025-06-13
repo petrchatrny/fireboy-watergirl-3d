095fc2d7c01596408bcf158b5b68a7b3cbf73f36
Petr Chatrný <posta@petrchatrny.cz>
2025-06-13 09:36:05 +0200

    Change liquids

--- Scenes/Environment/magma.gd
+++ Scenes/Environment/magma.gd
@@ -1,19 +1,6 @@
-extends StaticBody3D
+extends Area3D
 
-@export var type: String = "magma"  # Tento typ určuje, že jde o acid
-@onready var area: Area3D = $Area3D  # Připojení k Area3D pro detekci vstupů
+@export var liquid_type: String = "magma"
 
-func _ready() -> void:
-	# Pokud je to acid, připojí se signál
-	area.connect("body_entered", Callable(self, "_on_body_entered"))
-
-# Funkce pro zpracování kolize s postavou
-func _on_body_entered(body: Node) -> void:
-	if body is CharacterBody3D:
-		print("Body entered: ", body.name)  # Ladicí výpis pro kontrolu
-		if body.is_watergirl:
-			print("Watergirl touched acid")
-			body.die("Acid killed Watergirl!")  # Smrt pro Watergirl
-		elif not body.is_watergirl:
-			print("Fireboy touched acid")
-			body.die("Acid killed Fireboy!")  # Smrt pro Fireboy
+func get_liquid_type() -> String:
+	return liquid_type
