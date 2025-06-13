68844246e13fdd5496d80af413457d151c3dca69
Petr Chatrný <posta@petrchatrny.cz>
2025-06-13 09:29:57 +0200

    Refactor player

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 
+signal player_died()
+
 @export var is_watergirl: bool = true  # true = Watergirl, false = Fireboy
-var score: int = 0  # Skóre pro hráče
-var maxScore: int = 10  # Maximální skóre pro vítězství nebo jiný účel
 
 # ovládání
 @export var controller_id: int = -1  # -1 = klávesnice/myš, 0+ = gamepad ID
@@ -18,6 +18,7 @@ var jump_action := ""
 @export var jump_velocity = 5.5
 var grounded = false
 var is_jumping_now = false
+var is_dead = false
 
 # rotace kamery
 var yaw = 0.0
@@ -25,34 +26,12 @@ var pitch = 0.0
 var sensitivity = 0.003
 
 # části těla
-const WATERGIRL_MESH_PATH := "res://Scenes/watergirl_mesh.tscn"
-const FIREBOY_MESH_PATH := "res://Scenes/fireboy_mesh.tscn"
-
+const WATERGIRL_MESH_PATH := "res://Scenes/Player/watergirl_mesh.tscn"
+const FIREBOY_MESH_PATH := "res://Scenes/Player/fireboy_mesh.tscn"
 var character_mesh: Node3D
 @onready var step_cast = $StepCast
-@onready var acid_scene = preload("res://Scenes/Environment/acid.tscn")
-@onready var magma_scene = preload("res://Scenes/Environment/magma.tscn")
-@onready var water_scene = preload("res://Scenes/Environment/water.tscn")
-
-# Funkce pro přičítání skóre
-func add_score(points: int) -> void:
-	score += points
-	# Aktualizace skóre na UI (labelScore je připravený v base-level)
-	$"../../../../HUD/ScoreRect/Control/DiamondCountLabel".text = str(score) + "/" + str(maxScore)
-
-# Funkce pro nastavení maxScore podle počtu diamantů
-func set_max_score() -> void:
-	# Tady využíváme skupinu "diamonds", kterou přidáš k diamantům v editoru
-	#var diamonds = get_tree().get_nodes_in_group("diamonds")  # Všechny objekty ve skupině "diamonds"
-	#maxScore = diamonds.size()
-	#print("Max score set to: ", maxScore)
-	maxScore = 10
-	print("Max score set to: ", maxScore)
 
 func _ready() -> void:
-	# Nastavení maxScore podle počtu diamantů ve scéně
-	set_max_score()
-
 	# nastavení podle typu hráče
 	var mesh_scene = null
 	if is_watergirl:
@@ -69,15 +48,6 @@ func _ready() -> void:
 		walk_right_action = "fb_walk_right"
 		jump_action = "fb_jump"
 		mesh_scene = preload(FIREBOY_MESH_PATH)
-	
-	# Vytvoření instancí a jejich přidání do hlavní scény
-	var acid_instance = acid_scene.instantiate()
-	var magma_instance = magma_scene.instantiate()
-	var water_instance = water_scene.instantiate()
-
-	add_child(acid_instance)
-	add_child(magma_instance)
-	add_child(water_instance)
 	
 	# načtení textury postavy a přidání do stromu prvků
 	character_mesh = mesh_scene.instantiate()
@@ -124,10 +94,11 @@ func _physics_process(delta: float) -> void:
 	$AnimationTree.set("parameters/conditions/idle", input_direction == Vector2.ZERO and is_grounded())
 	$AnimationTree.set("parameters/conditions/walking", input_direction != Vector2.ZERO and is_grounded())
 	$AnimationTree.set("parameters/conditions/jumping", not is_grounded())
-	$AnimationTree.set("parameters/conditions/death", true)
+	$AnimationTree.set("parameters/conditions/dead", is_dead)
 
-	move(delta)
-	is_jumping_now = false
+	if not is_dead:
+		move(delta)
+		is_jumping_now = false
 
 # zachytávání vstupů na periferiích
 func _input(event: InputEvent) -> void:
@@ -143,30 +114,14 @@ func _input(event: InputEvent) -> void:
 	
 	rotate_camera()
 
-# Funkce pro zpracování kolize s diamantem
-# Funkce pro zpracování kolize s diamanty
-func _on_body_entered(body: Node) -> void:
-	if body is Area3D:
-		print("Body entered: ", body.name)
-		if is_watergirl:
-			if body.name == "water_diamond":
-				body.queue_free()
-				print("Watergirl picked up the blue diamond!")
-				add_score(1)
-		else:
-			if body.name == "fire_diamond":
-				body.queue_free()
-				print("Fireboy picked up the red diamond!")
-				add_score(1)
-
-	if body is StaticBody3D:
-		print("StaticBody3D entered: ", body.name)  # Ladicí výpis pro kontrolu
-		if is_watergirl:
-			if body.name == "acid" or body.name == "magma":
-				die("Watergirl touched acid or magma!")
-		else:
-			if body.name == "acid" or body.name == "water":
-				die("Fireboy touched acid or water!")
+# kontrola kolize s objekty
+func _on_liquid_detector_area_entered(area: Area3D) -> void:
+	if not area.has_method("get_liquid_type"):
+		return
+		
+	if should_die_from(area.get_liquid_type()):
+		is_dead = true
+		emit_signal("player_died")
 
 # pohyb v čase
 func move(delta: float):
@@ -228,17 +183,15 @@ func is_jumping() -> bool:
 func is_grounded() -> bool:
 	return grounded || is_on_floor()
 
-func die(cause: String) -> void:
-	print(cause)  # Debugging output
+# true pokud se hráč dotýká tekutiny, která ho zabije
+func should_die_from(liquid_type):
+	if liquid_type == "acid":
+		return true
+	if is_watergirl and liquid_type == "magma":
+		return true
+	if not is_watergirl and liquid_type == "water":
+		return true
+	return false
 
-	if $AnimationTree != null:
-		print("Setting death animation")
-		$AnimationTree.set("parameters/conditions/death", true)
-
-		velocity = Vector3.ZERO
-
-		print("Death animation triggered.")
-	else:
-		print("Error: AnimationTree is not set up.")
-
-	print("Game Over: " + cause)
+func is_water_girl():
+	return is_watergirl
