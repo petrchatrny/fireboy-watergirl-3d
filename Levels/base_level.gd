4afaef4d3b0bfc448e30f7adc3b4140a479f4ed8
Petr Chatrný <posta@petrchatrny.cz>
2025-06-13 09:37:26 +0200

    Improve base_level

--- Levels/base_level.gd
+++ Levels/base_level.gd
@@ -1,19 +1,26 @@
 extends Node
 
-var total_time_seconds : int = 0 
+var passed_seconds_count : int = 0
+var total_diamonds_count: int = 0
+var collected_diamonds_count: int = 0
+var is_game_over: bool = false
+var is_winner: bool = false
 
 @onready var watergirl = $SplitScreen/LeftScreen/SubViewport/Watergirl
 @onready var fireboy = $SplitScreen/RightScreen/SubViewport/Fireboy
 
-func _ready():
+func _on_ready() -> void:
+	# na základě připojených zařízení k PC vyhodnotí ovládání hry
 	setup_peripherals()
 	
-	# přepnutí myši do "InGame" režimu
-	# Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	# připojení signálů z diamantů
+	connect_diamond_signals()
 	
-	$SplitScreen/LeftScreen.focus_mode = Control.FocusMode.FOCUS_NONE
-	$SplitScreen/RightScreen.focus_mode = Control.FocusMode.FOCUS_ALL
-	$SplitScreen/RightScreen.grab_focus()
+	# uložení výchozího počtu diamantů do proměnné
+	total_diamonds_count = get_total_diamonds_count()
+	
+	# nastavení správného výchozího skóre
+	update_score()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -22,17 +29,27 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _on_timer_timeout() -> void:
 	# aktualizace proměnné
-	total_time_seconds += 1
+	passed_seconds_count += 1
 	
 	# vizuální vykreslení změny času
-	var minutes = int(total_time_seconds / 60)
-	var seconds = int(total_time_seconds % 60)
+	var minutes = int(passed_seconds_count / 60)
+	var seconds = int(passed_seconds_count % 60)
 	$HUD/TimerRect/TimeLabel.text = "%02d:%02d" % [minutes, seconds]
 
-# ======================================================================================
+func _on_diamond_collected():
+	collected_diamonds_count += 1
+	update_score()
+
+# ------------------------------------------------------------------------------
 
 # na základě připojených zařízení k PC vyhodnotí ovládání hry
 func setup_peripherals(): 
+	# přepnutí myši do "InGame" režimu
+	# Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	$SplitScreen/LeftScreen.focus_mode = Control.FocusMode.FOCUS_NONE
+	$SplitScreen/RightScreen.focus_mode = Control.FocusMode.FOCUS_ALL
+	$SplitScreen/RightScreen.grab_focus()
+	
 	var joypads = Input.get_connected_joypads()
 	
 	# dva ovladače - oba hráči na gamepadech
@@ -56,3 +73,22 @@ func setup_peripherals():
 		
 		fireboy.controller_id = -1
 		fireboy.use_mouse = true
+
+# připojí signály z diamantů, aby je level mohl přijímat
+func connect_diamond_signals():
+	var diamond_container = $Scene/Diamonds
+	for diamond in diamond_container.get_children():
+		if diamond.has_signal("diamond_collected"):
+			diamond.connect("diamond_collected", Callable(self, "_on_diamond_collected"))
+		else:
+			print("⚠️ Uzel ", diamond.name, " nemá signál 'collected'")
+
+# aktualizuje text skóre v HUD
+func update_score() -> void:
+	var text = str(collected_diamonds_count) + "/" + str(total_diamonds_count)
+	$HUD/ScoreRect/Control/DiamondCountLabel.text = text
+
+# získá počet diamantů
+func get_total_diamonds_count() -> int:
+	var diamond_container = $Scene/Diamonds
+	return diamond_container.get_child_count()
