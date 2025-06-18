dc01baa1a0a2dfd84c23db00de7d8995607f9f69
Petr Chatrný <posta@petrchatrny.cz>
2025-06-18 11:34:22 +0200

    Remove test levels

--- Levels/level_2.gd
+++ Levels/level_2.gd
@@ -6,13 +6,6 @@ func _handle_button_toggled(is_pressed: bool) -> void:
 	door_opened = is_pressed
 	
 	if(door_opened):
-		$Scene/LockableDoorWall2.open()
-	else:
-		$Scene/LockableDoorWall2.close()
-
-
-func _on_pressure_toggled(is_pressed: bool) -> void:
-	if is_pressed:
 		$Scene/Controls/LockableDoorWall.open()
 	else:
 		$Scene/Controls/LockableDoorWall.close()
--- Levels/test_level_2.gd
+++
@@ -1,1 +0,0 @@
-extends "res://Levels/base_level.gd"
