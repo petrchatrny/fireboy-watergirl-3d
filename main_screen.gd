247938608b13fc7b4dc0e6fd330bfcf2cef8786f
Lucie Suchankova <lucie.suchankova2@seznam.cz>
2025-06-21 14:18:11 -0800

    main_screen added

---
+++ main_screen.gd
@@ -0,0 +1,9 @@
+extends CenterContainer
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://level_select_screen.tscn")
+
+
+func _on_controls_button_pressed() -> void:
+	pass # Replace with function body.
+ 
